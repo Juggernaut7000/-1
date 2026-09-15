1e3d79d03e924780525846a6fe05eb7f167d0de8
Juggernaut7000 <alexrich2511@gmail.com>
2026-09-15 14:46:04 +0300

    Add commented code for lab assignment 1
    Added commented-out code for a lab assignment.

--- завдання 15 лабораторна 1.cpp
+++ завдання 15 лабораторна 1.cpp
@@ -12,3 +12,30 @@ cout << "A = " << A << endl;
 cout << "B = " << B << endl;
 cout << "C = " << C << endl;
 return 0; }
+
+
+/*
+#include <iostream> 
+using namespace std; 
+int main() {
+    
+    cout << "Лабораторна робота №1 гр.319а Заставського Назара" << endl;
+    cout << "" << endl;
+
+    int A, B, C, t; 
+ 
+    cout << "Введіть значення змінних A, B, C: ";
+    cin >> A >> B >> C; 
+    
+    t = A; A = B; B = C; C = t;
+    
+    cout << "Нові значення зміннних :" << endl;
+    
+    cout << "A = " << A << endl; 
+    cout << "B = " << B << endl;
+    cout << "C = " << C << endl;
+
+    return 0; 
+    
+}
+*/
